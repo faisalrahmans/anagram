3de88b0408347c371598a565f1c019d79ccf24e1
faisalrahman <faisalrahsu@gmail.com>
2021-07-08 23:52:07 +0700

    anagram

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const arr = ['kita', 'atik', 'tika', 'aku', 'kia', 'makan', 'kua']
+
+  let sortedArr = arr.map(item => item.split('').sort().join(''));
+  let setArr = new Set(sortedArr);
+  let reducedObj = {};
+  for (let setItem of setArr) {
+    let indexArr = sortedArr.reduce((acc, cur, index) => {
+      if (setItem === cur) {
+        acc.push(index);
+      }
+      return acc;
+    }, []);
+    reducedObj[setItem] = indexArr;
+  }
+  let finalArr = [];
+  for (let reduceItem in reducedObj) {
+    finalArr.push(reducedObj[reduceItem].map(item => arr[item]));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Anagram</h1>
+          <p>
+            {arr.map(item => {
+              return <span>{item}, </span>;
+            })}
+          </p>
+          <p>
+            {finalArr.map(item => {
+              return <li>{item.map(item => {
+                return <span>{item}, </span>
+              })}</li>;
+            })}
+          </p>
+        </div>
+        {/* <p>{word}</p> */}
       </header>
     </div>
   );
